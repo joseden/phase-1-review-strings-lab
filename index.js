16a871d3f2e651c8b9954411cc6cc1cda777df84
Joseph <joseph.dena@student.moringaschool.com>
2023-10-22 19:55:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+// Write your code in this file
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let let currentUser1  =currentUser.slice(0,1);
+
+let shortGreeting = `Welcome, ${currentUser1}!`;
+
+console.log(currentUser1);
+
+
+
+
+
